3dd84ad26b042b30c755346dca61e64d1e5f258e
Alexander Topilskii <a.topilskii@indriver.com>
2024-11-03 08:13:35 +0200

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.voyager.navigator)
+    implementation(libs.voyager.screenmodel)
+    implementation(libs.voyager.bottom.sheet.navigator)
+    implementation(libs.voyager.tab.navigator)
+    implementation(libs.voyager.transitions)
+    implementation(libs.voyager.livedata)
 }
