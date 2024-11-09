020e546ef515e8f112b986bd833b86bdcacbb7fe
Alexander Topilskii <a.topilskii@indriver.com>
2024-11-09 11:45:27 +0200

    init

--- app/src/main/java/com/ato/animations/simple_animation/SwipeHeightAnimation.kt
+++ app/src/main/java/com/ato/animations/simple_animation/SwipeHeightAnimation.kt
@@ -3,16 +3,16 @@ package com.ato.animations.simple_animation
 import android.content.res.Configuration
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.Spacer
@@ -22,8 +22,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,8 +35,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -48,94 +53,69 @@ fun PreviewSwipeHeightSample() {
     DisplaySwipeHeightSample()
 }
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DisplaySwipeHeightSample() {
     var boxHeight by remember { mutableStateOf(100.dp) }
-    var time by remember { mutableStateOf("500") }
     var stiffValue by remember { mutableStateOf("50") }
     var dumpValue by remember { mutableStateOf("0.5") }
 
-    var fastOutSlowInEasing by remember { mutableStateOf(true) }
-    var linearOutSlowInEasing by remember { mutableStateOf(false) }
-    var fastOutLinearInEasing by remember { mutableStateOf(false) }
-    var stiffnessLow by remember { mutableStateOf(false) }
+    val listState = rememberLazyListState()
 
-    val animationSpec = when {
-        fastOutSlowInEasing -> tween(
-            durationMillis = time.toIntOrNull() ?: 500,
-            easing = FastOutSlowInEasing
-        )
-
-        linearOutSlowInEasing -> tween(
-            durationMillis = time.toIntOrNull() ?: 500,
-            easing = LinearOutSlowInEasing
-        )
-
-        fastOutLinearInEasing -> tween(
-            durationMillis = time.toIntOrNull() ?: 500,
-            easing = FastOutLinearInEasing
-        )
-
-        stiffnessLow -> spring<Dp>(
-            dampingRatio = dumpValue.toFloatOrNull() ?: 0.5f,
-            stiffness = stiffValue.toFloatOrNull() ?: 50f
-        )
-
-        else -> tween(
-            durationMillis = time.toIntOrNull() ?: 500,
-            easing = LinearEasing
-        )
-    }
-
+    val animationSpec = spring<Dp>(
+        dampingRatio = dumpValue.toFloatOrNull() ?: 0.5f,
+        stiffness = stiffValue.toFloatOrNull() ?: 50f
+    )
     val animatedHeight: Dp by animateDpAsState(
         targetValue = boxHeight,
         animationSpec = animationSpec,
         label = "height animation"
     )
 
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val dragAmount = available.y
+
+                if (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0) {
+
+                    boxHeight += if (dragAmount < 0) -10.dp else 10.dp
+                    if (boxHeight < 50.dp) {
+                        boxHeight = 50.dp
+
+                        return available.copy(y = 0f)
+                    }
+                    if (boxHeight > 500.dp) {
+                        boxHeight = 500.dp
+
+                        return available.copy(y = 0f)
+                    }
+                } else {
+                    return Offset.Zero
+                }
+
+
+
+                return available
+            }
+        }
+    }
+
     Box(
         modifier = Modifier
+            .nestedScroll(nestedScrollConnection)
             .fillMaxSize()
     ) {
         LazyColumn(
+            state = listState,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 16.dp)
         ) {
-            item {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .height(300.dp)
-                        .pointerInput(Unit) {
-                            detectVerticalDragGestures { _, dragAmount ->
-                                boxHeight += if (dragAmount < 0) -10.dp else 10.dp
-                                if (boxHeight < 50.dp) boxHeight = 50.dp
-                                if (boxHeight > 500.dp) boxHeight = 500.dp
-                            }
-                        }
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .align(Alignment.TopCenter)
-                            .height(animatedHeight)
-                            .width(150.dp)
-                            .background(Color.Blue)
-                    )
-                }
+            stickyHeader {
+                MainBox(animatedHeight)
             }
             item {
                 Spacer(modifier = Modifier.height(8.dp))
-            }
-            item {
-                OutlinedTextField(
-                    value = time,
-                    onValueChange = { time = it },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    label = { Text("Speed: $time") }
-                )
             }
             item {
                 Spacer(modifier = Modifier.height(8.dp))
@@ -163,25 +143,101 @@ fun DisplaySwipeHeightSample() {
             item {
                 Spacer(modifier = Modifier.height(8.dp))
             }
-            item {
-                FlowColumn(
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    TextCheckBox("fastOutSlowInEasing", fastOutSlowInEasing) {
-                        fastOutSlowInEasing = !fastOutSlowInEasing
-                    }
-                    TextCheckBox("linearOutSlowInEasing", linearOutSlowInEasing) {
-                        linearOutSlowInEasing = !linearOutSlowInEasing
-                    }
-                    TextCheckBox("fastOutLinearInEasing", fastOutLinearInEasing) {
-                        fastOutLinearInEasing = !fastOutLinearInEasing
-                    }
-                    TextCheckBox("stiffnessLow", stiffnessLow) {
-                        stiffnessLow = !stiffnessLow
-                    }
-                }
+            items((0..100).map { "$it" }) { item ->
+                Text(
+                    text = item,
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
+        }
+    }
+}
+
+@Composable
+private fun getAnimation(
+    fastOutSlowInEasing: Boolean,
+    time: String,
+    linearOutSlowInEasing: Boolean,
+    fastOutLinearInEasing: Boolean,
+    stiffnessLow: Boolean,
+    dumpValue: String,
+    stiffValue: String
+): FiniteAnimationSpec<Dp> {
+    val animationSpec = when {
+        fastOutSlowInEasing -> tween(
+            durationMillis = time.toIntOrNull() ?: 500,
+            easing = FastOutSlowInEasing
+        )
+
+        linearOutSlowInEasing -> tween(
+            durationMillis = time.toIntOrNull() ?: 500,
+            easing = LinearOutSlowInEasing
+        )
+
+        fastOutLinearInEasing -> tween(
+            durationMillis = time.toIntOrNull() ?: 500,
+            easing = FastOutLinearInEasing
+        )
+
+        stiffnessLow -> spring<Dp>(
+            dampingRatio = dumpValue.toFloatOrNull() ?: 0.5f,
+            stiffness = stiffValue.toFloatOrNull() ?: 50f
+        )
+
+        else -> tween(
+            durationMillis = time.toIntOrNull() ?: 500,
+            easing = LinearEasing
+        )
+    }
+    return animationSpec
+}
+
+@Composable
+private fun MainBox(
+    animatedHeight: Dp
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .height(animatedHeight)
+                .fillMaxWidth()
+                .background(Color.Blue)
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalLayoutApi::class)
+private fun CheckBoxes(
+    fastOutSlowInEasing: Boolean,
+    linearOutSlowInEasing: Boolean,
+    fastOutLinearInEasing: Boolean,
+    stiffnessLow: Boolean
+) {
+    var fastOutSlowInEasing1 = fastOutSlowInEasing
+    var linearOutSlowInEasing1 = linearOutSlowInEasing
+    var fastOutLinearInEasing1 = fastOutLinearInEasing
+    var stiffnessLow1 = stiffnessLow
+    FlowColumn(
+        modifier = Modifier.padding(horizontal = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        TextCheckBox("fastOutSlowInEasing", fastOutSlowInEasing1) {
+            fastOutSlowInEasing1 = !fastOutSlowInEasing1
+        }
+        TextCheckBox("linearOutSlowInEasing", linearOutSlowInEasing1) {
+            linearOutSlowInEasing1 = !linearOutSlowInEasing1
+        }
+        TextCheckBox("fastOutLinearInEasing", fastOutLinearInEasing1) {
+            fastOutLinearInEasing1 = !fastOutLinearInEasing1
+        }
+        TextCheckBox("stiffnessLow", stiffnessLow1) {
+            stiffnessLow1 = !stiffnessLow1
         }
     }
 }
