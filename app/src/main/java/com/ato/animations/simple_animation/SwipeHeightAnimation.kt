dfb73450c542562e8721a2f73c8d552c6eeed412
Alexander Topilskii <a.topilskii@indriver.com>
2024-11-10 10:42:28 +0200

    init

--- app/src/main/java/com/ato/animations/simple_animation/SwipeHeightAnimation.kt
+++ app/src/main/java/com/ato/animations/simple_animation/SwipeHeightAnimation.kt
@@ -1,11 +1,9 @@
 package com.ato.animations.simple_animation
 
 import android.content.res.Configuration
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -15,11 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,7 +30,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.Velocity
@@ -50,115 +46,35 @@ fun PreviewSwipeHeightSample() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun DisplaySwipeHeightSample() {
-    val minHeight = 48.dp
-    val mediumHeight = 100.dp
-    val maxHeight = 200.dp
-
-    var boxHeight by remember { mutableStateOf(mediumHeight) }
-    var stiffValue by remember { mutableStateOf("50") }
-    var dumpValue by remember { mutableStateOf("0.9") }
-    var currentStep by remember { mutableStateOf(CurrentStep.MEDIUM) }
-
+fun DisplaySwipeHeightSample(
+    minHeight: Dp = 48.dp,
+    mediumHeight: Dp = 100.dp,
+    maxHeight: Dp = 200.dp,
+    animationSpec: AnimationSpec<Dp> = spring(
+        dampingRatio = 0.5f,
+        stiffness = 500f
+    )
+) {
+    var targetHeight by remember { mutableStateOf(mediumHeight) }
     val listState = rememberLazyListState()
 
-    val animationSpec = spring<Dp>(
-        dampingRatio = dumpValue.toFloatOrNull() ?: 0.9f,
-        stiffness = stiffValue.toFloatOrNull() ?: 500f
-    )
-    val tween = tween<Dp>(
-        durationMillis = 300,
-        easing = FastOutLinearInEasing
-    )
+    val nestedScrollConnection = remember {
+        NestedScrollAnimation(
+            listState = listState,
+            minHeight = minHeight,
+            mediumHeight = mediumHeight,
+            maxHeight = maxHeight,
+            onTargetHeightChanged = { targetHeight = it }
+        )
+    }
 
     val animatedHeight: Dp by animateDpAsState(
-        targetValue = boxHeight,
-        animationSpec = tween,
+        targetValue = targetHeight,
+        animationSpec = animationSpec,
         label = "height animation",
     )
 
-    val nestedScrollConnection = remember {
-        object : NestedScrollConnection {
-
-            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                val dragAmount = available.y
-
-                if (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0) {
-                    val nextTarget = boxHeight + dragAmount.dp
-
-                    when {
-                        // Если куда мы направляемся больше медиума но мы сейчас движемся из позиции Min
-                        nextTarget > mediumHeight && currentStep == CurrentStep.MIN -> {
-                            boxHeight = mediumHeight
-
-                            return available.copy(y = dragAmount)
-                        }
-
-                        currentStep == CurrentStep.MAX -> {
-                            boxHeight = minHeight
-                            return available.copy(y = dragAmount)
-                        }
-
-                        nextTarget < minHeight -> {
-                            boxHeight = minHeight
-                            return available.copy(y = 0f)
-                        }
-
-                        nextTarget > maxHeight -> {
-                            boxHeight = maxHeight
-                            return available.copy(y = 0f)
-                        }
-
-
-                        else -> {
-                            boxHeight = nextTarget
-                            return available.copy(y = dragAmount)
-                        }
-                    }
-                } else {
-                    return Offset.Zero
-                }
-            }
-
-            override suspend fun onPreFling(available: Velocity): Velocity {
-                if (animatedHeight != boxHeight) {
-                    // останавливаем Fling если человек бесконечно скроллит
-                    return available
-                }
-                return super.onPreFling(available)
-            }
-
-            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
-                val distanceToMax = abs(boxHeight.value - maxHeight.value)
-                val distanceToMedium = abs(boxHeight.value - mediumHeight.value)
-                val distanceToMin = abs(boxHeight.value - minHeight.value)
-
-                val epsilon = 1e-6f
-                when {
-                    abs(
-                        distanceToMin - minOf(distanceToMin, distanceToMedium, distanceToMax)
-                    ) < epsilon -> {
-                        boxHeight = minHeight
-                        currentStep = CurrentStep.MIN
-                    }
-
-                    abs(
-                        distanceToMedium - minOf(distanceToMin, distanceToMedium, distanceToMax)
-                    ) < epsilon -> {
-                        boxHeight = mediumHeight
-                        currentStep = CurrentStep.MEDIUM
-                    }
-
-                    else -> {
-                        boxHeight = maxHeight
-                        currentStep = CurrentStep.MAX
-                    }
-                }
-
-                return super.onPostFling(consumed, available)
-            }
-        }
-    }
+    nestedScrollConnection.animatedHeight = animatedHeight
 
     Box(
         modifier = Modifier
@@ -173,31 +89,7 @@ fun DisplaySwipeHeightSample() {
             stickyHeader {
                 MainBox(animatedHeight)
             }
-            item {
-                Spacer(modifier = Modifier.height(8.dp))
-            }
 
-            item {
-                OutlinedTextField(
-                    modifier = Modifier.padding(16.dp),
-                    value = stiffValue,
-                    onValueChange = { stiffValue = it },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-                    label = { Text("Stiff (0-10_000): $stiffValue") }
-                )
-            }
-            item {
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-            item {
-                OutlinedTextField(
-                    modifier = Modifier.padding(16.dp),
-                    value = dumpValue,
-                    onValueChange = { dumpValue = it },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-                    label = { Text("Dump (0-1f): $dumpValue") }
-                )
-            }
             item {
                 Spacer(modifier = Modifier.height(8.dp))
             }
@@ -211,6 +103,120 @@ fun DisplaySwipeHeightSample() {
         }
     }
 }
+
+class NestedScrollAnimation(
+    private val listState: LazyListState,
+    private val onTargetHeightChanged: (Dp) -> Unit,
+    private val minHeight: Dp = 48.dp,
+    private val mediumHeight: Dp = 100.dp,
+    private val maxHeight: Dp = 200.dp,
+) : NestedScrollConnection {
+
+     var animatedHeight: Dp = mediumHeight
+
+    private var targetHeight = mediumHeight
+        set(value) {
+            onTargetHeightChanged(value)
+            field = value
+        }
+    private var currentStep = CurrentStep.MEDIUM
+    private var goingToMin = false
+
+
+    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+        val dragAmount = available.y
+
+        if (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0) {
+            val nextTarget = targetHeight + dragAmount.dp
+
+            when {
+                // Если куда мы направляемся больше медиума но мы сейчас движемся из позиции Min
+                nextTarget > mediumHeight && currentStep == CurrentStep.MIN -> {
+                    targetHeight = mediumHeight
+                    goingToMin = false
+                    return available.copy(y = dragAmount)
+                }
+
+                currentStep == CurrentStep.MAX -> {
+                    targetHeight = minHeight
+                    goingToMin = false
+                    return available.copy(y = dragAmount)
+                }
+
+                nextTarget < minHeight && animatedHeight != targetHeight -> {
+                    targetHeight = minHeight
+                    goingToMin = true
+                    return available.copy(y = 0f)
+                }
+
+                nextTarget < minHeight && animatedHeight == targetHeight -> {
+                    targetHeight = minHeight
+                    goingToMin = true
+                    return available.copy(y = 0f)
+                }
+
+                nextTarget > maxHeight && animatedHeight != targetHeight -> {
+                    targetHeight = maxHeight
+                    goingToMin = false
+                    return available.copy(y = dragAmount)
+                }
+
+                nextTarget > maxHeight && animatedHeight == targetHeight -> {
+                    targetHeight = maxHeight
+                    goingToMin = false
+                    return available.copy(y = 0f)
+                }
+
+                else -> {
+                    targetHeight = nextTarget
+                    goingToMin = false
+                    return available.copy(y = dragAmount)
+                }
+            }
+        } else {
+            return Offset.Zero
+        }
+    }
+
+    override suspend fun onPreFling(available: Velocity): Velocity {
+        if (animatedHeight != targetHeight && !goingToMin) {
+            // останавливаем Fling если человек бесконечно скроллит
+            return available
+        }
+        return super.onPreFling(available)
+    }
+
+    override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+        val distanceToMax = abs(targetHeight.value - maxHeight.value)
+        val distanceToMedium = abs(targetHeight.value - mediumHeight.value)
+        val distanceToMin = abs(targetHeight.value - minHeight.value)
+
+        val epsilon = 1e-6f
+        when {
+            abs(
+                distanceToMin - minOf(distanceToMin, distanceToMedium, distanceToMax)
+            ) < epsilon -> {
+                targetHeight = minHeight
+                currentStep = CurrentStep.MIN
+            }
+
+            abs(
+                distanceToMedium - minOf(distanceToMin, distanceToMedium, distanceToMax)
+            ) < epsilon -> {
+                targetHeight = mediumHeight
+                currentStep = CurrentStep.MEDIUM
+            }
+
+            else -> {
+                targetHeight = maxHeight
+                currentStep = CurrentStep.MAX
+            }
+        }
+
+        return super.onPostFling(consumed, available)
+    }
+}
+
 
 enum class CurrentStep {
     MIN, MEDIUM, MAX
